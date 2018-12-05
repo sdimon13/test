c080d7373476d722f0e96e6813c04f6c850119c3
Суходольский Дмитрий Андреевич <d.sukhodolsky@id-network.ru>
2018-12-05 09:53:41 +0300

    Added middleware to ebay routing group

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('ebay', 'Ebay\EbayController@index');
-Route::get('ebay/add', 'Ebay\EbayController@findItemsAdvanced');
-Route::get('ebay/sellers', 'Ebay\EbayController@sellers');
-Route::get('ebay/products', 'Ebay\EbayController@products');
-Route::get('ebay/test', 'Ebay\EbayController@test');
+Route::group(['prefix' => 'ebay', 'namespace' => 'Ebay', 'middleware' => 'auth'], function () {
+    Route::get('/', 'EbayController@index');
+    Route::get('add', 'EbayController@findItemsAdvanced');
+    Route::get('sellers', 'EbayController@sellers');
+    Route::get('products', 'EbayController@products');
+    Route::get('test', 'EbayController@test');
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
